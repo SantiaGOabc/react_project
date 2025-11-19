24ad7d8d3b4ff37497e047b8f91066d2b3827365
matias_santiago <santiagomatriverop@gmail.com>
2025-11-18 23:02:17 -0400

    tarea

---
+++ backWeb/bd.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE IF NOT EXISTS perros;
+USE perros;
+CREATE TABLE perros.productos (
+	id_producto INTEGER auto_increment NOT NULL,
+	nombre varchar(100) NULL,
+	descripcion varchar(300) NULL,
+	imagen varchar(100) NULL,
+	estado varchar(1) NULL,
+	precio DECIMAL(10,2) NULL,
+	CONSTRAINT productos_pk PRIMARY KEY (id_producto)
+)
+ENGINE=MyISAM
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_general_ci;
+
+
+CREATE TABLE `contactos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `mensaje` text DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM CHARSET=utf8mb4;
